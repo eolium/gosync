3fe2637971092484e11699593c4882e991c6302f
Aweirdwhale <goussin.olivier@protonmail.com>
2025-01-20 20:51:02 +0100

    Test de compilation 1

--- main.go
+++ main.go
@@ -3,154 +3,170 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"flag" // pour les options dans la commande
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
-
-	_ "github.com/glebarez/go-sqlite"
 )
 
-/*
-Retourne le hash sha256 d'un fichier, fourni par son nom
-*/
-func filehash(filename string) (string, error) {
-	// On crée un buffer sha256, que l'on remplie avec la lecture du fichier
-	hasher := sha256.New()
-	s, err := os.ReadFile(filename)
-	if err != nil {
-		return "", err
+// Variable globale pour activer/désactiver le mode verbose
+var verbose bool
+
+// Affiche les messages seulement si le mode verbose est activé
+func logVerbose(format string, args ...interface{}) {
+	if verbose {
+		log.Printf(format, args...)
 	}
-
-	_, err = hasher.Write(s)
-	if err != nil {
-		return "", err
-	}
-
-	// On renvoie la somme, décodée depuis l'hexadécimal
-	h := hex.EncodeToString(hasher.Sum(nil))
-
-	return h, nil
 }
 
-/*
-Synchronise la création des dossiers de src vers dst
-*/
+// calcule de la signature SHA256 d'un fichier donné et retourne sa valeur hexadécimale.
+func filehash(filename string) (string, error) {
+	hasher := sha256.New()
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", fmt.Errorf("Échec d'ouverture du fichier : %w", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(hasher, file)
+	if err != nil {
+		return "", fmt.Errorf("Échec de calcul du hash : %w", err)
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+// Copie le fichier source vers sa destination.
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("Échec d'ouverture du fichier source %s : %w", src, err)
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("Échec de création du fichier destination %s : %w", dst, err)
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return fmt.Errorf("Échec de copie de %s vers %s : %w", src, dst, err)
+	}
+
+	logVerbose("Fichier %s copié vers %s\n", src, dst)
+	return nil
+}
+
+// syncFolder synchronise la structure des dossiers de src vers dst.
 func syncFolder(src string, dst string) error {
-	// On exécute pour chaque fichier/dossier récursivement
-	err := filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			return fmt.Errorf("Erreur lors de la navigation dans %s : %w", path, err)
 		}
 
-		// On récupère le chemin
 		rpath, err := filepath.Rel(src, path)
-
-		// Esquive les erreurs et la "recréation" dossier parent
 		if err != nil || rpath == "." {
 			return err
 		}
 
-		dst_path := filepath.Join(dst, rpath)
+		dstPath := filepath.Join(dst, rpath)
 
 		if info.IsDir() {
-			if _, err = os.Stat(dst_path); err != nil {
-				// Si le dossier n'existe pas, on le crée
-
-				err = os.MkdirAll(filepath.Join(dst, rpath), info.Mode())
+			if _, err := os.Stat(dstPath); os.IsNotExist(err) {
+				err := os.MkdirAll(dstPath, info.Mode())
 				if err != nil {
-					return err
+					return fmt.Errorf("Échec de création du dossier %s : %w", dstPath, err)
 				}
-
-				fmt.Println(rpath, "mis à jour")
+				logVerbose("Dossier %s synchronisé\n", rpath)
 			}
 		}
 
-		return err
+		return nil
 	})
-
-	return err
 }
 
+// sync synchronise les fichiers de src vers dst en vérifiant les différences de contenu.
 func sync(src string, dst string) error {
-	// On exécute pour chaque fichier/dossier récursivement
-	err := filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			return fmt.Errorf("Erreur lors de la navigation dans %s : %w", path, err)
 		}
-		// Pas d'erreurs précédentes
 
-		// On récupère le chemin
 		rpath, err := filepath.Rel(src, path)
-
-		// Esquive les erreurs et la "recréation" dossier parent
 		if err != nil || rpath == "." {
 			return err
 		}
 
-		dst_path := filepath.Join(dst, rpath)
+		dstPath := filepath.Join(dst, rpath)
 
 		if !info.IsDir() {
-			if _, err = os.Stat(dst_path); err != nil {
-				// Le fichier n'existe pas, on le copie
-				srcFile, _ := os.Open(path)
-				destFile, _ := os.Create(filepath.Join(dst, rpath))
-
-				_, err = io.Copy(destFile, srcFile)
+			if _, err := os.Stat(dstPath); os.IsNotExist(err) {
+				err := copyFile(path, dstPath)
+				if err != nil {
+					return err
+				}
+				logVerbose("Fichier %s copié\n", rpath)
+			} else {
+				srcHash, err := filehash(path)
+				if err != nil {
+					return err
+				}
+				dstHash, err := filehash(dstPath)
 				if err != nil {
 					return err
 				}
 
-				fmt.Println(rpath, "copié")
-			} else {
-				// Le fichier existe
-				a, _ := filehash(path)
-				b, _ := filehash(dst_path)
-
-				if a != b {
-					// Les hash sont différents, On maj le fichier
-					srcFile, _ := os.Open(path)
-					destFile, _ := os.Create(filepath.Join(dst, rpath))
-
-					_, err = io.Copy(destFile, srcFile)
+				if srcHash != dstHash {
+					err := copyFile(path, dstPath)
 					if err != nil {
 						return err
 					}
-
-					fmt.Println(rpath, "mis à jour")
+					logVerbose("Fichier %s mis à jour\n", rpath)
 				}
 			}
 		}
 
-		return err
+		return nil
 	})
-
-	return err
 }
 
+// main est le point d'entrée du programme.
 func main() {
-	if len(os.Args) < 3 {
-		// Pas assez d'arguments (compter ./gosync, SRC, DST)
-		fmt.Println(
-			"Usage : ./gosync [SRC1] [SRC2] ... [DST]\n",
-			"SRC : dossier à copier\n",
-			"DST : dossier destination",
-		)
+	// Définition de l'option verbose
+	flag.BoolVar(&verbose, "verbose", false, "Afficher des détails sur les fichiers transférés")
+	flag.BoolVar(&verbose, "v", false, "Alias pour --verbose")
+	flag.Parse()
+
+	// Vérifie que suffisamment d'arguments sont fournis
+	if flag.NArg() < 2 {
+		fmt.Println("Usage : ./gosync [options] [SRC1] [SRC2] ... [DST]")
+		fmt.Println("Options :")
+		fmt.Println("  --verbose, -v    Afficher des détails sur les fichiers transférés")
+		fmt.Println("SRC : dossier(s) à synchroniser")
+		fmt.Println("DST : dossier de destination")
 		return
 	}
 
-	dst := os.Args[len(os.Args)-1]
+	dst := flag.Arg(flag.NArg() - 1)
 
-	for i := 1; i < len(os.Args)-1; i++ {
-		src := os.Args[i]
+	for i := 0; i < flag.NArg()-1; i++ {
+		src := flag.Arg(i)
+
+		log.Printf("Synchronisation de %s vers %s\n", src, dst)
 
 		err := syncFolder(src, dst)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Échec de synchronisation des dossiers : %v\n", err)
 		}
 		err = sync(src, dst)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Échec de synchronisation des fichiers : %v\n", err)
 		}
 	}
+
+	log.Println("Synchronisation terminée avec succès.")
 }
